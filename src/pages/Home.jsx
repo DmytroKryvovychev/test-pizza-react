bb6f64df55f3b22fd6ea71b47f2d4006564f5349
Dmitro Krivovichev <angeldemon90@mail.ru>
2020-07-27 20:44:47 +0300

    Loading added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { Categories, SortPopup, PizzaBlock } from '../components';
+import { fetchPizzas } from '../redux/actions/pizzas';
+import { Categories, SortPopup, PizzaBlock, PizzaLoadingBlock } from '../components';
 import { setCategory } from '../redux/actions/filters';
 
 const categoryNames = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
@@ -15,6 +16,11 @@ const sortItems = [
 function Home() {
   const dispatch = useDispatch();
   const items = useSelector(({ pizzas }) => pizzas.items);
+  const isLoaded = useSelector(({ pizzas }) => pizzas.isLoaded);
+
+  React.useEffect(() => {
+    dispatch(fetchPizzas());
+  }, [dispatch]);
 
   const onSelectCategory = React.useCallback(
     (index) => {
@@ -31,7 +37,9 @@ function Home() {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {items && items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+        {isLoaded
+          ? items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
+          : Array(12).fill(<PizzaLoadingBlock />)}
       </div>
     </div>
   );
